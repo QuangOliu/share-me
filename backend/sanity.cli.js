3e3b92ec99188fc3473d5c118fdf82e38eb485af
Quang Oliu <trantran30102002@gmail.com>
2023-01-18 23:56:19 +0700

    init

---
+++ backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6qft97bh',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  tile: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'username',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
